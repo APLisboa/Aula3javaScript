3a3d7569157bdf1d13a6310b661cf2056d4e2890
Ana-PaulaPau316 <25146957-Ana-PaulaPau316@users.noreply.replit.com>
2023-08-03 10:24:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let numeroAStr = prompt('Digite o número A');
+let numeroBStr = prompt('Digite o número B');
+
+let numeroA = Number(numeroAStr);
+let numeroB = Number(numeroBStr);
+
+let resultado = numeroA + numeroB;
+alert(resultado);
